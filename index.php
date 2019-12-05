27d687755fc0b83c7b6d7a909a67de51b72576b3
M-Astrid <jean.jen@ya.ru>
2019-12-05 19:23:40 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: astrid
+ * Date: 05.12.2019
+ * Time: 9:28
+ */
+include "public/index.php";
---
+++ src/Controller/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="main")
+     */
+    public function index()
+    {
+        return new Response('<html><head></head><body>123</body></html>');
+        //return $this->render('main/index.html.twig', [
+        //    'controller_name' => 'MainController',
+        //]);
+    }
+}
